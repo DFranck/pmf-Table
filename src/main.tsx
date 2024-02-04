b607c96da8c1ebc30bfaad9e72917520a6d78b82
DFranck <franckdufournet@hotmail.fr>
2024-02-04 15:04:44 +0700

    je n'arrive pas a afficher les image via le link

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Table from "./Table";
+// import mock from "./mock/employee.json";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <>
+      {/* <Table data={mock} /> */}
       <Table />
     </>
   </React.StrictMode>
