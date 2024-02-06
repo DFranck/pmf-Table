c47f83f4c9ad4458629135f2be76b6f57960e859
DFranck <franckdufournet@hotmail.fr>
2024-02-06 16:55:59 +0700

    premier test pour verifier la config

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,9 @@
 /* eslint-env node */
 module.exports = {
-  // Liste des chemins des fichiers à tester
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
-  // Transformer les fichiers TypeScript avec ts-jest
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  // Définir le module mapper si nécessaire
-  moduleNameMapper: {
-    // Exemple pour les fichiers CSS
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  // Configuration spécifique à TypeScript
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-      // Autres options de ts-jest
-    },
-  },
-  setupFilesAfterEnv: ["./jest-setup.ts"],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/"],
 };
--- src/TableInfo/TableInfo.test.tsx
+++ src/TableInfo/TableInfo.test.tsx
@@ -1,28 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { TableInfo } from "./TableInfo";
-
-describe("TableInfo Component", () => {
-  it("displays correct range of entries when total length is greater than display length", () => {
-    const { getByText } = render(
-      <TableInfo
-        displayLength={10}
-        totalLength={100}
-        pageNumber={2}
-        data={[]}
-      />
-    );
-    expect(getByText("Showing 11 to 20 of 100 entries")).toBeInTheDocument();
-  });
-
-  test("displays correct range of entries when total length is less than display length", () => {
-    const { getByText } = render(
-      <TableInfo displayLength={10} totalLength={8} pageNumber={1} data={[]} />
-    );
-    expect(getByText("Showing 1 to 8 of 8 entries")).toBeInTheDocument();
-  });
-
-  test("displays filtered entries when total data length is greater than total length", () => {
-    const { getByText } = render(
+describe("TableInfo Render Gracefully", () => {
+  it("should render correctly", () => {
+    render(
       <TableInfo
         displayLength={10}
         totalLength={100}
@@ -30,8 +10,7 @@ describe("TableInfo Component", () => {
         data={[]}
       />
     );
-    expect(
-      getByText("(filtered from {totalDataLength} total entries)")
-    ).toBeInTheDocument();
+    const textElement = screen.getByText("Showing 1 to 10 of 100 entries");
+    expect(textElement).toBeInTheDocument();
   });
 });
