d8a9badecc08f12f6c981915b0c4ae946ae1d3f2
DFranck <franckdufournet@hotmail.fr>
2024-02-13 13:39:38 +0700

    fichier composant dans un dossier dedié

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Table from "./Table";
+import Table from "./components/Table";
 // import mock from "./mock/employee.json";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
