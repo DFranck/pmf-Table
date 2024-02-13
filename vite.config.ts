12cc1f162fd19eb03416ebdedf51714d3c2e01dc
DFranck <franckdufournet@hotmail.fr>
2024-02-13 17:06:52 +0700

    test viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   build: {
-    lib: {
-      entry: "src/Table.tsx",
-      formats: ["es"],
-      fileName: "pmf-table",
-    },
     rollupOptions: {
       input: {
-        main: "src/Table.tsx",
+        main: "src/main.tsx",
       },
     },
   },
